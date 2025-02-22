afa01aef29c1199ae6c5204fff4b6bb8ff2a0c33
juliochandra <juliochandra@gmail.com>
2025-02-22 11:34:03 +0700

    FIX media

--- src/api/media/route.js
+++ src/api/media/route.js
@@ -42,10 +42,10 @@ const upload = multer({
 
 const buildFileUrl = (req, file) =>
   // in development
-  // `${req.protocol}://${req.get("host")}/media/${file}`;
+  `${req.protocol}://${req.get("host")}/media/${file}`;
 
-  // in production use https
-  `https://${req.get("host")}/media/${file}`;
+// in production use https
+// `https://${req.get("host")}/media/${file}`;
 
 // Upload image
 router.post("/", authentication, upload.single("image"), (req, res) => {
