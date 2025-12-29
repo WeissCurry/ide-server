e8038692efc26aa57e25b8f6cdab3fe70a687aed
WeissCurry <Maulanasykari@gmail.com>
2025-12-29 16:50:33 +0700

    fix: deployment bugs

--- src/utilities/logging.js
+++ src/utilities/logging.js
@@ -13,25 +13,31 @@ const transportsConsole = new winston.transports.Console({
   ),
 });
 
-const transportsFile = new DailyRotateFile({
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
-    winston.format.printf((info) => {
-      return `${info.timestamp}\n${info.level} => ${info.message}\n`;
-    }),
-  ),
-  filename: "./logs/%DATE%-logger.log",
-  datePattern: "YYYY-MM-DD",
-  maxSize: "1kb",
-  maxFiles: "1d",
-});
+const transportList = [transportsConsole];
+
+if (process.env.NODE_ENV !== 'production') {
+  const transportsFile = new DailyRotateFile({
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss",
+      }),
+      winston.format.printf((info) => {
+        return `${info.timestamp}\n${info.level} => ${info.message}\n`;
+      }),
+    ),
+    filename: "./logs/%DATE%-logger.log",
+    datePattern: "YYYY-MM-DD",
+    maxSize: "1kb",
+    maxFiles: "1d",
+  });
+
+  transportList.push(transportsFile);
+}
 
 const logger = winston.createLogger({
   level: "info",
   levels: winston.config.npm.levels,
-  transports: [transportsConsole, transportsFile],
+  transports: transportList, 
 });
 
 module.exports = logger;
